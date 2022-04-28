4dab80b7080a4cc868b7a00fa720c5448511e0d3
Retr0ville <xclusivesam14@gmail.com>
2022-04-28 22:48:49 +0100

    feature: add responsive header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header';
 import './App.css';
 
+
+function InfoBody() {
+    return (<div className="body w-100 d-flex justify-content-center">
+                <div className="container mw-1100 mx-3 mx-lg-5 bg-color-secondary">
+                    text
+                </div>
+            </div>);
+}
+
+
 const Home = () => {
     return (
-        <div>
-            <Header />
-            <h1>Dictionary</h1>
+        <div className="">
+            <div className="bg-danger w-100 py-3 d-flex align-items-center justify-content-center">
+                <Header />
+            </div>
+           <InfoBody />
         </div>
     );
 };
