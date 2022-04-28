fe301abe0bac0b7a6f53205edac8ab682eea114c
Retr0ville <xclusivesam14@gmail.com>
2022-04-28 16:03:01 +0100

    rtrvl- initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React from 'react';
+import Header from './components/Header';
+import './App.css';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Dictionary</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React from 'react';
+
+const Header = () => {
+    return (
+    <div className="bg-primary w-100">
+      <h1>
+        Diction-io
+      </h1>
+    </div>
+    )
+};
+
+export default Header;
